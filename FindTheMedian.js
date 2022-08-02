85486f65bebbaa76df72dea807f791ebb2b0ba2d
Renan Martins <85807389+renanmdreis45@users.noreply.github.com>
2022-08-01 23:46:15 -0300

    Create FindTheMedian.js

---
+++ FindTheMedian.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'findMedian' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function findMedian(arr) {
+    let median, tam = arr.length;
+    arr.sort(function(a, b){return a-b});
+
+    median = arr[((tam+1)/2)-1]
+    console.log(median);
+    console.log(arr);
+    return median
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = findMedian(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
